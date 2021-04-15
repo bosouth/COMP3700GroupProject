d43e26c410740e87f3e08d0e1004664d8a703316
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-15 15:42:49 -0500

    Add files via upload

---
+++ InformationSystem.java
@@ -0,0 +1,12 @@
+public class InformationSystem {
+   
+   protected String userType;
+   protected int userName;
+   protected String password;
+   
+   public InformationSystem(String userTypeIn, int userNameIn, String passwordIn) {
+      userType = userTypeIn;
+      userName = userNameIn;
+      password = passwordIn;
+   }
+}
