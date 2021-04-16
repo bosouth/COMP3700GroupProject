3f02329d42a2d43561eb8dc3894788e88df19228
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-16 11:25:57 -0500

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,24 @@
+public class Report {
+   
+   protected String name;
+   
+   public Report(String nameIn) {
+      name = nameIn;
+   }
+   
+   public String getName() {
+      return name;
+   }
+   
+   public void create(String nameIn) {
+   
+   }
+   
+   public void save() {
+   
+   }
+   
+   public String print(String nameIn) {
+      return "";
+   }
+}
