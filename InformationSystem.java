5f0241d988ddfae931e808e17076b1ac4f48240e
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-16 10:00:31 -0500

    Update InformationSystem.java

--- InformationSystem.java
+++ InformationSystem.java
@@ -1,12 +1,28 @@
 public class InformationSystem {
    
    protected String userType;
-   protected int userName;
+   protected String userName;
    protected String password;
    
-   public InformationSystem(String userTypeIn, int userNameIn, String passwordIn) {
+   public InformationSystem(String userTypeIn, String userNameIn, String passwordIn) {
       userType = userTypeIn;
       userName = userNameIn;
       password = passwordIn;
+   }
+   
+   public void login(String usernameIn, String passwordIn) {
+   
+   }
+   
+   public void logout(String usernameIn, String passwordIn) {
+   
+   }
+   
+   public void search() {
+   
+   }
+   
+   public void register() {
+   
    }
 }
