6030ff55dbbf5a201126e15d04a4af12546d5edb
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-19 17:42:19 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,34 +1,16 @@
 public class Account extends InformationSystem {
 
-   protected String type;
-   protected int itemsBorrowed;
-   protected int itemsReserved;
-   protected int itemsLost;
-   
-   public Account(String userTypeIn, String usernameIn, String passwordIn, String typeIn, int itemsBorrowedIn, int itemsReservedIn, int itemsLostIn) {
+   public int id;
+    
+   public Account(String userTypeIn, String usernameIn, String passwordIn, int idIn) {
       super(userTypeIn, usernameIn, passwordIn);
-      type = typeIn;
-      itemsBorrowed = itemsBorrowedIn;
-      itemsReserved = itemsReservedIn;
-      itemsLost = itemsLostIn;
-   } 
-   
-   public String getType() {
-      return type;
+      id = idIn;
    }
    
-   public int getItemsBorrowed() {
-      return itemsBorrowed;
-   }
-   
-   public int getItemsReserved() {
-      return itemsReserved;
-   }
-   
-   public int getItemsLost() {
-      return itemsLost;
-   }
-   
+   public int getId() {
+      return id;
+   }   
+      
    public void deactivate() {
    
    }
