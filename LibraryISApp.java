5e528867a6dc17c766e936a4f5e68d0f8e7ddeb2
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-19 13:25:37 -0500

    Update LibraryISApp.java

--- LibraryISApp.java
+++ LibraryISApp.java
@@ -7,6 +7,7 @@ public class LibraryISApp {
    public static void main(String args[]) {
    
       LibraryDatabase db = new LibraryDatabase("book", "0721");
+      String code = "";
    
       Scanner userInput = new Scanner(System.in);
       boolean registered = false;
@@ -16,34 +17,66 @@ public class LibraryISApp {
       
       String userType = userInput.nextLine();
       
-      // enter user info
-      System.out.println("Great! Now enter your username and password to get started.");
+      System.out.println("Great! Here's the library menu: \n"
+         + "L - log in\n"
+         + "R - register\n"
+         + "Q - quit");
+         
       
-      System.out.print("Username: ");
-      String username = userInput.nextLine();
-      System.out.print("Password: ");
-      String password = userInput.nextLine();
-      
-      InformationSystem cust = new InformationSystem(userType, username, password);
-      
-      //check if user is in the library database
-      if (!cust.login(username, password)) {
-         System.out.println("\nUser is not in the system. Please register an account.\n");
-         //if not, register
-         // user will input username and password, will be given a 4 digit Library ID
-         // system will notify user of ID assignment and whenever they are ready to check out
-         if (cust.register() == true) {
-            int id = cust.setLibraryId(username);
-            System.out.println("\nYour ID is: " + id);
-            System.out.println("\nYou are ready to start checking out.");
+      do {
+         if (registered) {
+            break;
          }
-      }
+         System.out.print("Enter Code [L, R, Q]: ");
+         code = userInput.nextLine();
+         if (code.length() == 0) {
+            continue;
+         } 
+         code = code.toUpperCase();
+         char codeChar = code.charAt(0);
       
-      db.initDatabase();
+         switch (codeChar) {
+            case 'L':
+             System.out.println("Enter your username and password to get started.");
       
-      System.out.print("Search for an item: ");
-      String itemType = userInput.nextLine();
+             System.out.print("Username: ");
+             String username = userInput.nextLine();
+             System.out.print("Password: ");
+             String password = userInput.nextLine();
+             
+             InformationSystem temp = new InformationSystem(userType, username, password);
+             
+             boolean loginStatus = temp.login(username, password);
+             
+             if (loginStatus == true) {
+               registered = true;
+             }
+               break;
       
+             case 'R':
+                System.out.print("Enter your user type: ");
+                userType = userInput.nextLine();
+                System.out.print("Enter your username: ");
+                username = userInput.nextLine();
+                System.out.print("Enter your password: ");
+                password = userInput.nextLine();
+                
+                temp = new InformationSystem(userType, username, password);
+                temp.initInfoSystem();
+
+                if (temp.register() == true) {
+                   int id = temp.setLibraryId(username);
+                   System.out.println("\nYour ID is: " + id);
+                   System.out.println("\nYou are ready to start checking out.");
+                   registered = true;
+                }
+                  break;
+               
+            default:
+               System.out.println("****Invalid code****\n");
+               break;  
+         }
+      } while (!code.equalsIgnoreCase("Q") && registered == false);
       // user will search for an item -- syatem will notify them whether or not it is available
       
       // system will notify whenever an item is added to the cart
@@ -61,5 +94,5 @@ public class LibraryISApp {
       // cue library employee to log in and print a report
      
       
-   }
+    }
 }
