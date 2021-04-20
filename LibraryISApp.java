792422b753c15d20662ed2dc3b433bf55ea4dd16
Liping98 <82613484+Liping98@users.noreply.github.com>
2021-04-20 12:20:47 -0500

    Update LibraryISApp.java

--- LibraryISApp.java
+++ LibraryISApp.java
@@ -126,18 +126,76 @@ public class LibraryISApp {
         }
       } while (!code.equalsIgnoreCase("Q")); 
       
-      // system will notify whenever an item is added to the cart
+       // system will notify whenever an item is added to the cart
+       if(cart.contains(search)) {
+           System.out.print("This item is added to the cart")
+       }
+       else {
+           System.out.print("This item is not added to the cart")
+       }
       // system will also keep track of all transactions to make a report later. 
       
       // user will proceed to check out -- all necessary conditions will be checked first
-      
-      // system will notify of anything that needs to be done before checking out
-      
-      // once checked out, the item will be taken off the database until it is returned
+
+           System.out.print("Please check out the necessary information");
+           System.out.print("The number of item is " + cart.length);
+           int i = 0;
+           int overall = 0;
+           while(i <= cart.lenggth) {
+               System.out.print("The " + i + " th item is " + cart[i]);
+
+               System.out.print("The available date of this item is " + cart[i].avaDate);
+               System.out.print("The price is " + cart[i].price);
+               overall += cart[i].price;
+               // once checked out, the item will be taken off the database until it is returned
+               cart[i].state == 0; // the item have two state, one is active,which can be checkout, the other state is inactive ,which cannot be chekout
+               i++;
+           }
+           System.out.println("The overall price is " + overall);
+       // system will notify of anything that needs to be done before checking out
+       boolean ch = true;
+       do {
+           System.out.println("Please check the account information");
+           System.out.ptintln("The User's address is " + acc.address()
+                   +"\nThe User's phone number is " + acc.phoneNum()
+                   + "\nThe User's email is " + acc.email()
+                   + "\n Is this true or false");
+           ch = userInput.nextLine();
+           if(ch == false) {
+               Syste.out.println("The updated address is " );
+               string add = userInput.nextLine();
+               acc.address() = add;
+               Syste.out.println("The updated phone number is " );
+               int phoneN = userInput.nextInt();
+               acc.phoneNum() = PhoneN;
+               Syste.out.println("The updated email is ");
+               String ema = userInput.nextLine();
+               acc.email() = ema;
+           }
+
+       } while(ch == true)
+
       // system will once again notify the user whenever checkout is successful
-      
+      System.out.println("The checkout is successful");
       // user will log out
-      
+       System.out.println("Retun to the menu or log out from system"
+               + "L - log out\n"
+               + "R - return to the menu\n");
+       code = userInput.nextLine();
+       if (code.length() == 0) {
+           continue;
+       }
+       code = code.toUpperCase();
+       char codeChar = code.charAt(0);
+       switch (codeChar) {
+           case 'L':
+               System.out.print("Log out the account");
+               break;
+           case 'R'
+                System.out.print("Return to the menu");
+                break;
+           default:
+       }
       // cue library employee to log in and print a report
      
       
