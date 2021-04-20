bfd7d881909fd1ae68e8a9ae7c05c7422419825b
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-20 13:44:37 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Customer extends User {
 
    protected String creditCard;
+   private Scanner scanner = new Scanner(System.in);
    
    public Customer(String userTypeIn, String usernameIn, String passwordIn, int idIn, String nameIn, String addressIn, int phoneNumIn, String emailIn, String creditCardIn) {
       super(userTypeIn, usernameIn, passwordIn, idIn, nameIn, addressIn, phoneNumIn, emailIn);
@@ -17,5 +20,25 @@ public class Customer extends User {
    
    public void payFees() {
       
+   }
+   
+   public void setAddress() {
+      System.out.println("Please enter a valid address: ");
+      this.address = scanner.nextLine();
+   }
+   
+   public void setPhoneNum() {
+      System.out.println("Please enter a phone number (7 digits): ");
+      this.phoneNum = Integer.parseInt(scanner.nextLine());
+   }
+   
+   public void setEmail() {
+      System.out.println("Please enter a valid email address: ");
+      this.email = scanner.nextLine();
+   }
+   
+   public void setCreditCard() {
+      System.out.println("Please enter a valid credit card number: ");
+      this.creditCard = scanner.nextLine();
    }
 }
