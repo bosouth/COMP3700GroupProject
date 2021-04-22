d69775482da35982d74ccde9835e04ded895f5fd
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-21 19:56:49 -0500

    Update Report.java

--- Report.java
+++ Report.java
@@ -2,23 +2,13 @@ public class Report {
    
    protected String name;
    
+   // cosntructor for Report
    public Report(String nameIn) {
       name = nameIn;
    }
    
+   // returns name for report
    public String getName() {
       return name;
-   }
-   
-   public void create(String nameIn) {
-   
-   }
-   
-   public void save() {
-   
-   }
-   
-   public String print(String nameIn) {
-      return "";
    }
 }
