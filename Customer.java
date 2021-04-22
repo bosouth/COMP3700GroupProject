d888759a89350db4624b081dcd650c83df0c9e2c
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-21 19:53:09 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -5,40 +5,38 @@ public class Customer extends User {
    protected String creditCard;
    private Scanner scanner = new Scanner(System.in);
    
+   // constructor for Customer
    public Customer(String userTypeIn, String usernameIn, String passwordIn, int idIn, String nameIn, String addressIn, int phoneNumIn, String emailIn, String creditCardIn) {
       super(userTypeIn, usernameIn, passwordIn, idIn, nameIn, addressIn, phoneNumIn, emailIn);
       creditCard = creditCardIn;
    }
    
+   // returns credit card number
    public String getCreditCard() {
       return creditCard;
    }
    
-   public int register(int idIn) {
-      return 0;
-   }
-   
-   public void payFees() {
-      
-   }
-   
+   // sets address for customer
    public void setAddress() {
-      System.out.println("Please enter a valid address: ");
+      System.out.print("Please enter a valid address: ");
       this.address = scanner.nextLine();
    }
    
+   // sets phone number for customer
    public void setPhoneNum() {
-      System.out.println("Please enter a phone number (7 digits): ");
+      System.out.print("Please enter a phone number (7 digits): ");
       this.phoneNum = Integer.parseInt(scanner.nextLine());
    }
    
+   // sets email address for customer
    public void setEmail() {
-      System.out.println("Please enter a valid email address: ");
+      System.out.print("Please enter a valid email address: ");
       this.email = scanner.nextLine();
    }
    
+   // sets credit card number for customer
    public void setCreditCard() {
-      System.out.println("Please enter a valid credit card number: ");
+      System.out.print("Please enter a valid credit card number: ");
       this.creditCard = scanner.nextLine();
    }
 }
