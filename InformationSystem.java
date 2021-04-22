e451c5c3a56693caef6e273b433ecdc90577a61a
bosouth <82606856+bosouth@users.noreply.github.com>
2021-04-21 19:54:28 -0500

    Update InformationSystem.java

--- InformationSystem.java
+++ InformationSystem.java
@@ -7,33 +7,41 @@ public class InformationSystem {
    protected String userType;
    protected String username;
    protected String password;
-   private static InformationSystem[] userDatabase;
-   private static int count;
+   public InformationSystem[] userDatabase;
+   public int infoCount;
    private boolean registered;
+   public boolean loginStatus;
+   
    private int id;
    
+   // comstructor of InformationSystem
    public InformationSystem(String userTypeIn, String usernameIn, String passwordIn) {
       userType = userTypeIn;
       username = usernameIn;
       password = passwordIn;
-      userDatabase = new InformationSystem[20];
-      count = 0;
+      userDatabase = new Account[20];
+      infoCount = 0;
       registered = false;
+      loginStatus = false;
       id = 0;
    }
    
+   // returns user type of the user
    public String getUserType() {
       return userType;
    }
    
+   // returns username of the user
    public String getUsername() {
       return username;
    }
    
+   // returns password of the user 
    public String getPassword() {
       return password;
    }
    
+   // randomly generates a library id
    public int setLibraryId(String usernameIn) {
       Random rand = new Random();
       
@@ -41,52 +49,36 @@ public class InformationSystem {
       return id;
    }
    
-   public boolean login(String usernameIn, String passwordIn) {
-      InformationSystem[] temp = initInfoSystem();
+   // checks to make sure that the username exists in the databse the password matches
+   public boolean login(String usernameIn, String passwordIn, InformationSystem[] userIn) {
       
-      for (InformationSystem user : temp) {
+      for (InformationSystem user : userIn) {
          if (user == null) {
             return false;
          }
          if (usernameIn.equals(user.username)) {
-            if (user.getPassword() != passwordIn) {
+            if (!user.getPassword().equals(passwordIn)) {
                System.out.println("Wrong password. Try again.\n");
+               return false;
             }
             else {
+               loginStatus = true;
                return true;
             }
          }
          else {
             System.out.println("Username doesn't exist in the system. Please register an account.\n");
+            return false;
          }
       }
       
       return false;
-   
-//       if (this.search(usernameIn)) {
-//          if (this.getPassword() != passwordIn) {
-//             System.out.println("Wrong password. Try aagin.");
-//             return false;
-//          }
-//          else {
-//             return true;
-//          }
-//       }
-//       else {
-//          System.out.println("Username doesn't exist in the system. Please register an account.\n");
-//          return false;
-//       }
-   }
-   
-   public void logout(String usernameIn, String passwordIn) {
-   
    }
    
    /** searches the database for input username
    * returns false if username doesn't exist
    * returns true if username exists */
    public boolean search(String usernameIn) {
-      initInfoSystem();
    
       for (int i = 0; i < userDatabase.length; i++) {
          if (userDatabase[i] == null) {
@@ -96,48 +88,19 @@ public class InformationSystem {
             return true;
          }
       }
-   
-//       if (this.getUsername() == usernameIn) {
-//          return true;
-//       }
-   
       return false;
    }
    
-   public boolean register() {
-      this.initInfoSystem();
+   // makes sure that the usernameIn doesn't exist 
+   public boolean register(String usernameIn) {
       
-      if (this.search(username) == false) {
-         InformationSystem temp = new InformationSystem(userType, username, password);
-         
-         System.out.println("Success! You have successfully been registered.");
-         
-         userDatabase[count] = temp;
-         count++;
-         
+      if (this.search(usernameIn) == false) {
+         System.out.println("Success! You have successfully been registered.\n");
          return true;
       }
       else {
          System.out.println("Username already exists in the database. Please try a different one.\n");
          return false;
       }
-   }
-   
-   public InformationSystem[] initInfoSystem() {
-      InformationSystem molly = new Account("Customer", "molly", "molly375", 3333);
-      userDatabase[count] = molly;
-      count++;
-      InformationSystem jacob = new Account("Customer", "jacob", "jacob112233", 2345);
-      userDatabase[count] = jacob;
-      count++;
-      InformationSystem billy = new Account("Customer", "billy", "billy28375", 9375);
-      userDatabase[count] = billy;
-      count++;
-      
-      return userDatabase;
-   }
-   
-
-   
-
+   } 
 }
